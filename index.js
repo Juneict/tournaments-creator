718de84871e5a85525e636eff28d117da5ea4de9
Juneict <junemoenyinyiict@gmail.com>
2025-02-28 16:42:18 +0700

    fix : vercel deploy

--- index.js
+++ index.js
@@ -22,7 +22,6 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use(cookieParser());
 app.set('view engine', 'ejs');
 app.set('views', path.join(__dirname, 'views'));
-app.use(expressLayouts);
 
 // Set user for all routes
 app.use(setUser);
